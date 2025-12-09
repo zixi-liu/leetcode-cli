ea4cc440621b6e8332d660cada85a565931932d9
Zixi Liu <zixi_liu@Zixis-MacBook-Pro.local>
2025-12-08 16:53:52 -0800

    167

--- tests/167.py
+++ tests/167.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left = 0
         right = len(numbers) - 1
-        print(right)
+        
         while left < right:
             if numbers[left] + numbers[right] == target:
                 break
